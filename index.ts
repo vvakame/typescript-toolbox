bf3420c2ed005d420531d25ee258d94070c676dd
vvakame <vvakame+dev@gmail.com>
2014-02-15 12:08:15 +0900

    Trimmed the appearance

---
+++ index.ts
@@ -0,0 +1,5 @@
+export import TypeScript = require("./typescript/tss");
+
+export import formatter = require("./src/formatter");
+export import syntaxTree = require("./src/syntaxTree");
+export import compiler = require("./src/compiler");
