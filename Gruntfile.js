fb91a25f41c641b3825be03256ce350c1761ef1e
vvakame <vvakame+dev@gmail.com>
2014-11-20 21:49:15 +0900

    update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,89 +5,37 @@ module.exports = function (grunt) {
 		ts: {
 			options: {
 				compile: true,                 // perform compilation. [true (default) | false]
-				comments: false,               // same as !removeComments. [true | false (default)]
+				comments: true,                // same as !removeComments. [true | false (default)]
 				target: 'es5',                 // target javascript language. [es3 (default) | es5]
 				module: 'commonjs',            // target javascript module style. [amd (default) | commonjs]
-				noImplicitAny: false,
-				sourceMap: false,              // generate a source map for every output js file. [true (default) | false]
+				noImplicitAny: true,
+				sourceMap: true,               // generate a source map for every output js file. [true (default) | false]
 				sourceRoot: '',                // where to locate TypeScript files. [(default) '' == source ts location]
 				mapRoot: '',                   // where to locate .map.js files. [(default) '' == generated js location.]
 				declaration: false             // generate a declaration .d.ts file for every output js file. [true | false (default)]
 			},
-			clientMain: {
+			main: {
 				src: ['index.ts']
 			},
-			clientTest: {
+			test: {
 				src: ['tests/mainTest.ts']
 			}
 		},
 		tslint: {
 			options: {
-				formatter: "prose",
-				configuration: {
-					// https://github.com/palantir/tslint#supported-rules
-					"rules": {
-						"bitwise": true,
-						"classname": true,
-						"curly": true,
-						"debug": false,
-						"dupkey": true,
-						"eofline": true,
-						"eqeqeq": true,
-						"evil": true,
-						"forin": false, // TODO 解消方法よくわからない
-						// "indent": [false, 4], // WebStormのFormatterと相性が悪い
-						"labelpos": true,
-						"label-undefined": true,
-						// "maxlen": [false, 140],
-						"noarg": true,
-						"noconsole": [false,
-							"debug",
-							"info",
-							"time",
-							"timeEnd",
-							"trace"
-						],
-						"noconstruct": true,
-						"nounreachable": false, // switch で怒られるので
-						"noempty": false, // プロパティアクセス付き引数有りのコンストラクタまで怒られるので
-						"oneline": [true,
-							"check-open-brace",
-							"check-catch",
-							"check-else",
-							"check-whitespace"
-						],
-						"quotemark": [true, "double"],
-						"radix": false, // 10の基数指定するのめんどいので
-						"semicolon": true,
-						"sub": true,
-						"trailing": true,
-						"varname": false, // _hoge とかが許可されなくなるので…
-						"whitespace": [false, // WebStormのFormatterと相性が悪い
-							"check-branch",
-							"check-decl",
-							"check-operator",
-							"check-separator" ,
-							"check-type"
-						]
-					}
-				}
+				configuration: grunt.file.readJSON("tslint.json")
 			},
-			files: {
-				src: ['lib/**/*.ts', 'test/**/*.ts']
+			base: {
+				src: [
+					'lib/**/*.ts',
+					'tests/**/*.ts'
+				]
 			}
 		},
-		tsd: {
-			client: {
+		dtsm: {
+			main: {
 				options: {
-					// execute a command
-					command: 'reinstall',
-
-					//optional: always get from HEAD
-					latest: false,
-
-					// optional: specify config file
-					config: './tsd.json'
+					config: "dtsm.json"
 				}
 			}
 		},
@@ -105,9 +53,8 @@ module.exports = function (grunt) {
 			}
 		},
 		clean: {
-			tsd: {
+			typings: {
 				src: [
-					// tsd installed
 					"typings/"
 				]
 			},
@@ -134,16 +81,16 @@ module.exports = function (grunt) {
 	});
 
 	grunt.registerTask(
-		'setup',
-		['clean:tsd', 'tsd']);
+			'setup',
+			['clean:typings', 'dtsm']);
 
 	grunt.registerTask(
-		'default',
-		['clean:clientScript', 'ts:clientMain', 'tslint']);
+			'default',
+			['clean:clientScript', 'ts:main', 'tslint']);
 
 	grunt.registerTask(
-		'test',
-		['clean:clientScript', 'ts', 'tslint', 'espower', 'mochaTest']);
+			'test',
+			['clean:clientScript', 'ts', 'tslint', 'espower', 'mochaTest']);
 
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	require('load-grunt-tasks')(grunt);
 };
